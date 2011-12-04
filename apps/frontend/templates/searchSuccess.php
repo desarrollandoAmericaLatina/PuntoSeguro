47d299d277199a8aa31dfc7f04b57a10c949abcf
Cristobal Miranda <cristobal.miranda.tt@gmail.com>
2011-12-04 12:39:24 -0300

    12:39PM

--- apps/frontend/templates/searchSuccess.php
+++ apps/frontend/templates/searchSuccess.php
@@ -1,20 +1,19 @@
-<div style="float:left; position:relative;left:200px;width:400px;">
-<h1 style="color:#DA5D13;position:relative;left:-140px;">Bienvenido a Punto Seguro </h1>
-<h2 style="height:70px;word-spacing:1em;">Una aplicación para compartir información sobre la seguridad de tu barrio y comuna.</h2>
-</div>
-<img src="/PuntoSeguro/web/images/logo_solo_b.jpg" class="logo" style="z-index:2;"/>
+
+<h1>Bienvenido a Punto Seguro,</h1>
+<h2>Una aplicación para compartir información sobre la seguridad de tu barrio y comuna.</h2>
+<img src="/PuntoSeguro/web/images/logo_solo_b.jpg" class="logo"/>
 
 <?php slot('title') ?>
   <?php  echo sprintf('Puntos en '.$address) ?>
 <?php end_slot(); ?>
-<div style="position:relative;top:-150px;">
+
 <?php include_partial('buscadorSuperior',array('address' => $address, 'categories'=>$categories, 'filterCatArray'=>$filterCatArray)) ?>
-</div>
-<div class="searchResults grid_9" id="map_container" style="position:relative;top:-120px;z-index:20;">
+
+<div class="searchResults grid_9" id="map_container">
     <div id="map_canvas">Cargando...</div>
 </div>
 
-<div class=" grid_3">
+<div class=" grid_3" >
 <?php // echo image_tag('loading-image.gif','style=float:left;')?>
 
 				
@@ -23,27 +22,6 @@
 
 </div>
 
-<div id="footer_">
-<h2 style="color:#DA5D13">Instrucciones:</h2>
-<ul>
-<li>1-Crea un punto seguro en el mapa en la ubicación de tu barrio.
-o Unete a un punto seguro ya creado.</li>
-<li>2-Discute, organizate con tus vecinos</li>
-<li>3.Infórmate sobre la delincuencia en tu comuna</li>
-<li>4.Comparte y difunde el punto seguro.</li>
-</ul>
-
-<h2 style="color:#DA5D13">Crea un punto seguro.</h2>
-
-<p>Un punto seguro es una página para compartir ideas sobre la seguridad o un tema de delincuencia en tu barrio.</p>
-<li>1.Coloca un título descriptivo</li>
-<li>2.Describe el tema a tratar y dale una función: prevenir, denunciar o pedir apoyo.</li>
-<li>3.Ingresa la dirección del punto seguro y elige comuna.
-*si fuese necesario, haz click sobre el ícono Punto Seguro y muevelo al punto deseado.</li>
-<li>4.Ingresa a tu Punto seguro y comparte tu opinión y dudas sobre tu tema.</li>
-</ul>
-
-</div>
 <script type="text/javascript">
 //<![CDATA[
 var SEARCH_RESULTS = {
