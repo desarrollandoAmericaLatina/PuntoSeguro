c9355bf5734077630fab852509f337e7e2e318cd
Cristobal Miranda <cristobal.miranda.tt@gmail.com>
2011-12-04 00:33:53 -0300

    pagina de inicio

--- web/GUI/q.php
+++ web/GUI/q.php
@@ -1,1 +1,20 @@
+<?
+$f = @$_GET['f'];
+if($f){echo '<a href="#" style="font-size:10px;float:right;" onclick="show_hide(\'trel\')">[X]</a>';}
+switch($f){
+	case 'logout':
+		//session_destroy();
+		header("Location: enter.php");
+		break;
+	case 'ingresara_punto':
+		echo '<h2>Ingresa a punto</h2>';
+		break;
+	case 'crear_punto':
+		echo '<h2>Crear punto</h2>';
+		break;
+	
+	
+}
 
+
+?>
